b051fb612a4202e63600758e5b60ea9f98cb5f61
Bodo Schönfeld <bodo.schoenfeld@niftycode.de>
2024-01-26 12:28:27 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# system-info-tool
+
+Mit diesem Programm können Systeminformationen (macOS, Linux, Windows) gelesen werden. Als Programmiersprache wird Python verwendet.
+
+Eine vereinfachte Version existiert auch im Repository [small-python-projects](https://github.com/niftycode/small-python-projects).
+
+Dieses Programm dient derzeit lediglich zu Schulungszwecken. 
+
+This program can be used to read system information (macOS, Linux, Windows). Python is used as the programming language.
+
+A simplified version also exists in my [small-python-projects](https://github.com/niftycode/small-python-projects) repository.
+
+This program is in progress and for training purposes only.
---
+++ src/main.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+"""
+Entry point of this program
+Version: 1.0
+Python 3.12
+Date created: October 5th, 2023
+Date modified: -
+"""
+
+import logging
+import os
+import pprint
+
+from logging.config import fileConfig
+
+from src import system_info
+
+# Add logger config
+fileConfig("logging.ini")
+logger = logging.getLogger()
+
+
+def main():
+    system_env = os.environ
+
+    print("System Info:")
+    print("------------")
+    system_info.fetch_system_info()
+
+    print(system_env["SHELL"])
+
+    print()
+    print("User Info:")
+    print("------------")
+    system_info.fetch_user_info()
+    '''
+    system_env = os.environ
+    # pprint.pprint(dict(system_env))
+
+    # Show home directory
+    print(system_env["HOME"])
+
+    # Show user
+    print(system_env["USER"])
+
+    # Show language
+    # print(system_env["LANG"])
+
+    # Show used shell
+    print(system_env["SHELL"])
+
+    try:
+        print(system_env["BASH"])
+    except KeyError as e:
+        print("ERROR: Environment variable does not exist!")
+        print(e)
+
+    # Or use get() instead of an exception handling.
+    '''
+
+if __name__ == '__main__':
+    main()
